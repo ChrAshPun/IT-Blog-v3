cd570d6e3c9b9d098ab2636717b79ef6e076cbe7
ChrAshPun <punlac1222@gmail.com>
2023-04-21 13:52:50 -0400

    added red nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
       <div className={styles.Content}>
         <h1>Welcome to my IT Blog</h1>
         <p>This website is a collection of technical issues that I troubleshooted 
-          through my career in the IT industry. Scroll down to see how I resolved 
+          throughout my career in the IT industry. Scroll down to see how I resolved 
           common IT problems or select a category from the navigation menu.</p>
       </div>
     </div>
